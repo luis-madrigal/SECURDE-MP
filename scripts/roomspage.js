38f15bc0640c1cb3861bfc3172bf2608f50cadc2
Neil Romblon <neilromblon@live.com>
2017-07-16 14:39:11 +0800

    added roompage functionality
    -not yet complete no db integration

--- scripts/roomspage.js
+++ scripts/roomspage.js
@@ -2,8 +2,11 @@ var roomName = "Meeting Room";
 var rowNum = 5;
 var numInterval = 26;
 
+
+
 function init(){
 	populateAvailable();
+
 }
 
 function addRow(parent,roomnum){
@@ -12,14 +15,18 @@ function addRow(parent,roomnum){
 	
 	var line = "<tr> <td>"+roomName +" "+ roomnum + "</td>";
 	
-
+	var h,suffix,info;
 	for(var i=0;i<numInterval-1;i++){
-		line+="<td> <div id ='"+roomid+"-"+i+ "' class='time-btn'>&nbsp;</div>"; //first interval (XX:00-XX:29)
+		h = (i/2)+7;
+		suffix = h < 12? 'AM' : 'PM';
+		h = ((h + 11) % 12 + 1);
+		info = "Meeting Room "+roomnum+" | "+h+":00-"+h+":29 "+suffix;
+		line+="<td> <div id ='"+roomid+"-"+i+ "' class='time-btn' title='Reserve "+info+"' onclick='clickedTimebtn(this,\""+info+"\")'>&nbsp;</div>"; //first interval (XX:00-XX:29)
 		i++;
-		line+="<div id ='"+roomid+"-"+i+ "' class='time-btn'>&nbsp;</div></td>"; //2nd interval (XX:30-XX:59)
+		info = "Meeting Room "+roomnum+" | "+h+":30-"+h+":59 "+suffix;
+		line+="<div id ='"+roomid+"-"+i+ "' class='time-btn' title='Reserve "+info+"' onclick='clickedTimebtn(this,\""+info+"\")'>&nbsp;</div></td>"; //2nd interval (XX:30-XX:59)
 	}
-	line.concat("</tr>");
-	console.log(line);
+	line+="</tr>";
 	parent.append(line);
 }
 
@@ -28,4 +35,11 @@ function populateAvailable(){
 	for(var i=1;i<=rowNum;i++){
 		addRow(tableBody,i);
 	}
+}
+
+function clickedTimebtn(source,schedDetails){
+	var parent = $("#"+source.id);
+	console.log(schedDetails);
+	parent.toggleClass("selected");
+	
 }
